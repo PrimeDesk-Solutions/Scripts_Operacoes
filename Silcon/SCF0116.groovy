1a68d7b9f802e0d839bd4eeb19c7c7990da48522
PrimeDesk-Solutions <leonardosuporte2@outlook.com>
2026-06-23 17:09:13 -0300

    Corrigido validação da operação quando não preenchida no documento

--- Silcon/SCF0116.groovy
+++ Silcon/SCF0116.groovy
@@ -28,9 +28,10 @@ public class Script extends sam.swing.ScriptBase {
             TableMap tmUserBaixa = executarConsulta(sql)[0] == null ? new TableMap() : executarConsulta(sql)[0];
             Long idUserBaixa = tmUserBaixa.getLong("userbaixa");
 
+            if(idUserBaixa == null) return;
+
             String nomeUsuarioBaixa = buscarNomeUsuarioBaixa(idUserBaixa);
 
-            if(idUserBaixa == null) return;
             if (idUserBaixa != obterUsuarioLogado().getAab10id() && !obterUsuarioLogado().aab10custom) throw new ValidacaoException("O usuário logado não pode estornar esse documento, solicitar ao usuario " + nomeUsuarioBaixa + " para efeturar o estorno.")
 
         } catch (Exception e) {
@@ -39,46 +40,54 @@ public class Script extends sam.swing.ScriptBase {
     }
 
     private Long buscarIdDocumento() {
-        MNavigation nvgAbb10codigo = getComponente("nvgAbb10codigo");
-        MNavigation nvgAbe01codigo = getComponente("nvgAbe01codigo");
-        MNavigation nvgAah01codigo = getComponente("nvgAah01codigo");
-        MTextFieldInteger txtAbb01num = getComponente("txtAbb01num");
-        MTextFieldString txtAbb01serie = getComponente("txtAbb01serie");
-        MTextFieldLocalDate txtAbb01data = getComponente("txtAbb01data");
-        MTextFieldBigDecimal txtAbb01valor = getComponente("txtAbb01valor");
-        MTextFieldString txtAbb01parcela = getComponente("txtAbb01parcela");
-        MTextFieldInteger txtAbb01quita = getComponente("txtAbb01quita");
+       try{
+           MNavigation nvgAbb10codigo = getComponente("nvgAbb10codigo");
+           MNavigation nvgAbe01codigo = getComponente("nvgAbe01codigo");
+           MNavigation nvgAah01codigo = getComponente("nvgAah01codigo");
+           MTextFieldInteger txtAbb01num = getComponente("txtAbb01num");
+           MTextFieldString txtAbb01serie = getComponente("txtAbb01serie");
+           MTextFieldLocalDate txtAbb01data = getComponente("txtAbb01data");
+           MTextFieldBigDecimal txtAbb01valor = getComponente("txtAbb01valor");
+           MTextFieldString txtAbb01parcela = getComponente("txtAbb01parcela");
+           MTextFieldInteger txtAbb01quita = getComponente("txtAbb01quita");
 
-        String idOper = nvgAbb10codigo.getNavigationController().getValue() != null ? nvgAbb10codigo.getNavigationController().getValue().getAbb10id() : "";
-        String idEntidade = nvgAbe01codigo.getNavigationController().getValue() != null ? nvgAbe01codigo.getNavigationController().getValue().getAbe01id() : "";
-        String idTipoDoc = nvgAah01codigo.getNavigationController().getValue() != null ? nvgAah01codigo.getNavigationController().getValue().getAah01id() : "";
-        String numDoc = txtAbb01num.getValue();
-        String serie = txtAbb01serie.getValue() != null ? txtAbb01serie.getValue() : "";
-        String data = txtAbb01data.getValue();
-        String valor = txtAbb01valor.getValue();
-        String parcela = txtAbb01parcela.getValue();
-        String quita = txtAbb01quita.getValue();
+           String idOper = nvgAbb10codigo.getNavigationController().getValue() != null ? nvgAbb10codigo.getNavigationController().getValue().getAbb10id() : "";
+           String idEntidade = nvgAbe01codigo.getNavigationController().getValue() != null ? nvgAbe01codigo.getNavigationController().getValue().getAbe01id() : "";
+           String idTipoDoc = nvgAah01codigo.getNavigationController().getValue() != null ? nvgAah01codigo.getNavigationController().getValue().getAah01id() : "";
+           String numDoc = txtAbb01num.getValue();
+           String serie = txtAbb01serie.getValue() != null ? txtAbb01serie.getValue() : "";
+           String data = txtAbb01data.getValue();
+           String valor = txtAbb01valor.getValue();
+           String parcela = txtAbb01parcela.getValue();
+           String quita = txtAbb01quita.getValue();
 
-        String whereSerie = txtAbb01serie.getValue() != null ? " AND abb01serie = '" + serie + "' " : "";
+           String whereSerie = txtAbb01serie.getValue() != null ? " AND abb01serie = '" + serie + "' " : "";
+           String whereOperCod = nvgAbb10codigo.getNavigationController().getValue() != null ? " AND abb01operCod = " + idOper : "";
+           String whereTipoDoc = " AND abb01tipo = " + idTipoDoc;
+           String whereNumDoc = " AND abb01num = " + numDoc;
+           String whereEntidade = " AND abb01ent = " + idEntidade;
+           String whereData = " AND abb01data = '" + data + "' ";
+           String whereValor = " AND abb01valor = " + valor;
+           String whereParcela = " AND abb01parcela = '" + parcela + "' ";
+           String whereQuita = " AND abb01quita = " + quita;
 
-        String sql = "SELECT daa01id " +
-                " FROM daa01 " +
-                " INNER JOIN abb01 ON daa01central = abb01id " +
-                " WHERE abb01operCod = " + idOper +
-                " AND abb01tipo = " + idTipoDoc +
-                " AND abb01num = " + numDoc +
-                " AND abb01ent = " + idEntidade +
-                " AND abb01data = '" + data + "' " +
-                " AND abb01valor = " + valor +
-                " AND abb01parcela = '" + parcela + "' " +
-                whereSerie +
-                " AND abb01quita = " + quita;
 
-        TableMap tmIdDoc = executarConsulta(sql)[0];
+           String sql = "SELECT daa01id " +
+                   " FROM daa01 " +
+                   " INNER JOIN abb01 ON daa01central = abb01id " +
+                   " WHERE TRUE "+
+                   whereOperCod + whereTipoDoc + whereNumDoc +
+                   whereEntidade + whereData + whereValor +
+                   whereParcela + whereSerie + whereQuita;
 
-        if(tmIdDoc == null) throw new ValidacaoException("Script: Não foi encontrado documento com os filtros informados.")
+           TableMap tmIdDoc = executarConsulta(sql)[0];
 
-        return tmIdDoc.getLong("daa01id");
+           if(tmIdDoc == null) throw new ValidacaoException("Script: Não foi encontrado documento com os filtros informados.")
+
+           return tmIdDoc.getLong("daa01id");
+       }catch (Exception e){
+           throw new ValidacaoException("Script: Falha ao buscar documento financeiro." + e.getMessage())
+       }
     }
     private String buscarNomeUsuarioBaixa(Long idUserBaixa){
         String sql = "SELECT aab10user FROM aab10 WHERE aab10id = " + idUserBaixa;
