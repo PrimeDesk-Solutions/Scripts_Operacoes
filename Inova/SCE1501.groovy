c876c16ebad0506cff69af4476b382b620c218a9
PrimeDesk-Solutions <leonardosuporte2@outlook.com>
2026-09-25 11:18:20 -0300

    Desenvolvido script SCE1501.groovy

--- Inova/SCE1501.groovy
+++ Inova/SCE1501.groovy
@@ -1,1 +1,15 @@
+import multitec.swing.components.MCheckBox
+import multitec.swing.core.MultitecRootPanel;
+import multitec.swing.components.MPanelCheckMPMS;
 
+
+public class Script extends sam.swing.ScriptBase{
+    @Override
+    public void execute(MultitecRootPanel tarefa) {
+        MPanelCheckMPMS pnlChkMPMS = getComponente("pnlChkMPMS");
+        MCheckBox chkItem = getComponente("chkItem");
+        chkItem.setValue(1);
+        pnlChkMPMS.getChkMaterial().setValue(1);
+        pnlChkMPMS.getChkProduto().setValue(1);
+    }
+}
